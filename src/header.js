9440673c4eaa2b4e65439f30c4c3b1c60b6a957e
coder-sanju <sanju2help@gmail.com>
2021-02-13 01:08:03 +0530

    Project setup

---
+++ src/header.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Deatils extends React.Component{
+    constructor()
+    {
+    super();
+    console.warn("hyy warning here ")
+    }
+    render()
+    {
+        console.warn("new warningn here")
+        return <h1> sanju welcom here  </h1>;
+        
+    }
+}
+
+
+
+
+
+export default Deatils;
